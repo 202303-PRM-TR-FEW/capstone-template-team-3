edcc03b6f66a0561158f77a6605c509c85b86d76
Berkay Bideci <berkaybideci@gmail.com>
2023-06-30 17:52:10 +0300

    Organizing Structure Fixes hunctasci/capstone-template-team-3#5

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import ExampleComponent from "./components/ExampleComponent/ExampleComponent";
+
+const Home = () => {
+    return (
+        <main className="example">
+            <h1>Home Component</h1>
+            <ExampleComponent />
+        </main>
+    );
+};
+
+export default Home;
