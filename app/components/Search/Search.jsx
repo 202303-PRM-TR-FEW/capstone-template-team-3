7b62b0f559fdaa989f6de7a9b9c762f3b1ba63f6
Burcu Bayraö <burju7@yahoo.com>
2023-07-03 13:40:59 +0300

    Added search bar comp and worked on alignment.

---
+++ app/components/Search/Search.jsx
@@ -0,0 +1,15 @@
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <form>
+      <input
+        type="text"
+        className="search bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Search for projects"
+      />
+    </form>
+  );
+};
+
+export default Search;
