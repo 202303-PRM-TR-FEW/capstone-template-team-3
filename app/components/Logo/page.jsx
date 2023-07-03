9b37b241f9e062189f8d25300e17872e4884f41e
Burcu Bayraö <burju7@yahoo.com>
2023-07-03 11:56:20 +0300

    worked on logo

---
+++ app/components/Logo/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "styles/logo.module.css";
+
+const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <p>Givingly</p>
+    </div>
+  );
+};
+
+export default Logo;
--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import NavLink from "../NavLink/NavLink";
+import Logo from "../Logo/page";
 const Navbar = () => {
-    return (
-        <nav>
-            <NavLink to='/' name='Home'/>
-            <NavLink to='/my-projects' name='My Projects'/>
-            <NavLink to='/profile' name='Profile'/>
-        </nav>
-    );
+  return (
+    <nav>
+      <Logo />
+      <NavLink to="/" name="Home" />
+      <NavLink to="/my-projects" name="My Projects" />
+      <NavLink to="/profile" name="Profile" />
+    </nav>
+  );
 };
 
 export default Navbar;
