8972cb67263ceb0e69c9b253436040ee8ce5efa9
Berkay Bideci <berkaybideci@gmail.com>
2023-07-04 15:37:41 +0300

    Navigation Page Fixes hunctasci/capstone-template-team-3#14
    Feat: add Navigation Page and its styling

--- app/navigation/page.jsx
+++ app/navigation/page.jsx
@@ -3,35 +3,36 @@ import "./navigation.css"
 const Navigation = () => {
     return (
         <main className="navigation-main">
-            <section className="w-2/3">
-                <p className="option-paragraph">I want to:</p>
-                <div className="flex justify-evenly items-center p-10 text-left">
-                    <input type="checkbox" name="support" id="support" className="w-[48px] h-[48px] rounded-xl text-theme outline-none focus:outline-none" />
-                    <div className="flex flex-col">
-                        <span className="support-main">Support</span>
-                        <span className="support-appendix">other projects</span>
+            <section className="checkbox-section">
+                <p className="checkbox-option-paragraph-text">I want to:</p>
+                <div className="checkbox-support-container">
+                    <input type="checkbox" name="support" id="support" className="checkbox-input" />
+                    <div className="checkbox-text-container">
+                        <h2 className="checkbox-header-text">Support</h2>
+                        <span className="checkbox-appendix-text">other projects</span>
                     </div>
                 </div>
-                <div className="flex justify-evenly items-center p-10 text-left">
-                    <input type="checkbox" name="support" id="support" className="w-[48px] h-[48px] rounded-xl text-theme" />
-                    <div className="flex flex-col">
-                        <span className="support-main">Kick-off</span>
-                        <span className="support-appendix">my project</span>
+                <hr className="navigation-divider" />
+                <div className="checkbox-kick-off-container">
+                    <input type="checkbox" name="support" id="support" className="checkbox-input" />
+                    <div className="checkbox-text-container">
+                        <h2 className="checkbox-header-text">Kick-off</h2>
+                        <span className="checkbox-appendix-text">my project</span>
                     </div>
                 </div>
             </section>
-            <section className="w-1/3">
-                <div className="flex flex-col justify-between bg-black rounded-2xl relative">
+            <section className="newsletter-section">
+                <div className="newsletter-container">
                     <div>
-                        <div className="bg-theme m-7 rounded-2xl h-[22.5em] w-5/6 mx-auto overflow-visible">
-                            <img src="https://assets.api.uizard.io/api/cdn/stream/e0453b28-0e1c-49c7-a0bc-629da3689327.png" alt="" className="absolute inset-5 w-11/12" />
+                        <div className="newsletter-banner-container">
+                            <img src="https://assets.api.uizard.io/api/cdn/stream/e0453b28-0e1c-49c7-a0bc-629da3689327.png" alt="Illustration of a hand holding green hearts near books." className="newsletter-banner" />
                         </div>
                     </div>
-                    <div className="px-10 py-5 text-accent">
-                        <h3 className="text-left inform-text my-5">Stay informed</h3>
-                        <p>Want to be among the first people to know about amazing projects on our platform? Join our monthly digest of the best causes.</p>
-                        <button className="bg-theme px-2 py-3 mt-10 text-black w-full rounded-2xl">Join newsletter</button>
-                    </div>
+                    <article className="newsletter-text-container">
+                        <h3 className="newsletter-header-text">Stay informed</h3>
+                        <p className="newsletter-paragraph-text">Want to be among the first people to know about amazing projects on our platform? Join our monthly digest of <br />the best causes.</p>
+                    </article>
+                    <button className="newsletter-button">Join newsletter</button>
                 </div>
             </section>
         </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        krona: ['Krona One', 'sans-serif']
+        krona: ['Krona One', 'sans-serif'],
+        mulish: ['Mulish', 'sans-serif']
       },
       colors: {
         "theme": "rgb(212, 238, 38)",
