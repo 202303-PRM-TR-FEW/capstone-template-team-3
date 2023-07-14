b3b5dc56da23c40e8473dc25334e0eff2e7a1dac
Hunc Tasci <hunc.tasci@gmail.com>
2023-07-14 22:19:33 +0300

    added some all the necessary info

--- app/campaigns/[campaignId]/page.jsx
+++ app/campaigns/[campaignId]/page.jsx
@@ -1,4 +1,6 @@
 import projects from "@/app/data/data";
+import Image from "next/image";
+import { FaRegCalendarDays } from "react-icons/fa6";
 
 // export async function generateStaticParams() {
 //   return projects.map((project) => ({
@@ -8,13 +10,43 @@ import projects from "@/app/data/data";
 // }
 
 export default function CampaignPage({ params }) {
-  const project = projects.find((p) => p.id === parseInt(params.Id));
-
-  console.log(project);
+  const project = projects.find((p) => p.id === parseInt(params.campaignId));
+  console.log(params);
   return (
-    <div>
-      <h1>This is the campaign page for campaign: {project.title}</h1>
-      <p>Title: {project.title}</p>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center space-y-5">
+        <div className="flex flex-col items-center justify-center">
+          <Image width={600} height={200} src={project.img} />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center space-y-3">
+          <h1 className="text-4xl font-bold">{project.title}</h1>
+          <h3>{project.organizer ? project.organizer : "Organizer"}</h3>
+        </div>
+        <div>
+          <h4>About project</h4>
+          <p>{project.about}</p>
+        </div>
+        <div>
+          <div>
+            <div>
+              <h5>Raised:</h5>
+              <p>{"$" + project.raised}</p>
+            </div>
+            <div>
+              <h5>Goal:</h5>
+              <p>{"$" + project.goal}</p>
+            </div>
+          </div>
+          <div>
+            {/* <Bar /> */}
+            <h5>
+              <FaRegCalendarDays /> {project.date}
+            </h5>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
