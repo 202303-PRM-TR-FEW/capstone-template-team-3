c932ac45591d13d35e03f32312ec84b45e827561
Burcu Bayraö <burju7@yahoo.com>
2023-07-18 15:23:30 +0300

    Refactor: changed icon's color and created css file for class properties. Moved css structure to seperate css file

--- app/components/CategoryFilter/CategoryIcons/AllIcon.jsx
+++ app/components/CategoryFilter/CategoryIcons/AllIcon.jsx
@@ -1,11 +1,12 @@
 import { FaGlobe } from "react-icons/fa6";
+import "../CategoryIcons/CategoryIcons.css"
 
 const AllIcon = () => {
   return (
-    <div className="flex flex-col items-center space-y-2">
+    <div className="icon-main">
       <FaGlobe
         size={40}
-        className="border-2 border-spacing-3 border-white rounded-md hover:text-black hover:border-black "
+        className="icon"
       />
       <span className="text-xs">All</span>
     </div>
