8859dcda45c0d591b02dc994fc4a46ea1ae14f2b
Abdulrahman Alsamaraie <64977909+AbdalrahmanM@users.noreply.github.com>
2023-07-19 12:54:05 +0300

    Conflit solved

--- app/components/CategoryFilter/CategoryFilter.jsx
+++ app/components/CategoryFilter/CategoryFilter.jsx
@@ -18,15 +18,9 @@ const CategoryFilter = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col p-3 m-5 space-y-4 container w-11/12 mx-auto">
-      <h2 className="text-2xl font-bold">Categories</h2>
-      <div className="flex flex-row items-center gap-4 text-white">
-=======
     <div className="category-main">
       <h2 className="category-text">Categories</h2>
       <div className="category-icons">
->>>>>>> 3259732726f7d04e0a8fe07e77fd4ea62ec9351e
         <AllIcon onClick={() => handleClick("all")} />
         <EducationIcon onClick={() => handleClick("education")}/>
         <CultureIcon onClick={() => handleClick("culture")} />
