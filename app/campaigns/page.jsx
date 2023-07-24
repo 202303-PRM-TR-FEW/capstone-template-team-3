9d1c98e1aa1d910cbfafd3d9d42ac194bb0cb695
Hunc Tasci <hunc.tasci@gmail.com>
2023-07-24 16:15:54 +0300

    retrieved campaigns data from firebase

--- app/campaigns/page.jsx
+++ app/campaigns/page.jsx
@@ -1,21 +1,45 @@
+"use client";
 import CategoryFilter from "../components/CategoryFilter/CategoryFilter";
 import LargeCard from "../components/LargeCard/LargeCard";
 import Card from "../components/Card/Card.jsx";
-import projects from "app/data/data.jsx";
+import {
+  collection,
+  getDoc,
+  query,
+  querySnapshot,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "../firebase/firebase";
+import { useEffect, useState } from "react";
 
 const Campaigns = () => {
+  const [campaigns, setCampaigns] = useState([]);
+
+  // Read campaigns from database
+  useEffect(() => {
+    const q = query(collection(db, "campaigns"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let campaignsArr = [];
+
+      querySnapshot.forEach((campaign) => {
+        campaignsArr.push({ ...campaign.data(), id: campaign.id });
+      });
+      setCampaigns(campaignsArr);
+    });
+  }, []);
+
   return (
     <main>
       <LargeCard />
       <CategoryFilter />
       <div className="flex flex-row gap-4 flex-wrap items-center justify-evenly container w-11/12 mx-auto">
-        {projects.map((project, index) => (
+        {campaigns.map((campaign, index) => (
           <div key={index}>
             <Card
-              img={project.img}
-              title={project.title}
-              raised={project.raised}
-              goal={project.goal}
+              img={campaign.img}
+              title={campaign.title}
+              raised={campaign.raised}
+              goal={campaign.goal}
             />
           </div>
         ))}
--- app/firebase/firebase.jsx
+++ app/firebase/firebase.jsx
@@ -1,17 +1,32 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, addDoc, collection, onSnapshot, query, where, getDoc, doc, setDoc } from "@firebase/firestore"
-import { GoogleAuthProvider, signInWithPopup, GithubAuthProvider, TwitterAuthProvider } from "firebase/auth";
-import {getStorage} from 'firebase/storage'
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  where,
+  getDoc,
+  doc,
+  setDoc,
+} from "@firebase/firestore";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  GithubAuthProvider,
+  TwitterAuthProvider,
+} from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // import { getAnalytics } from "firebase/analytics";
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    updateProfile,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,20 +34,20 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCmOR3vfocrPF0U3vtZFxHTZAHGVFh2wCA",
-    authDomain: "givingly-fbfdf.firebaseapp.com",
-    projectId: "givingly-fbfdf",
-    storageBucket: "givingly-fbfdf.appspot.com",
-    messagingSenderId: "783237880324",
-    appId: "1:783237880324:web:0378e0dc2c8a1ab0ab3351",
-    measurementId: "G-S146F8DGPQ"
+  apiKey: "AIzaSyCmOR3vfocrPF0U3vtZFxHTZAHGVFh2wCA",
+  authDomain: "givingly-fbfdf.firebaseapp.com",
+  projectId: "givingly-fbfdf",
+  storageBucket: "givingly-fbfdf.appspot.com",
+  messagingSenderId: "783237880324",
+  appId: "1:783237880324:web:0378e0dc2c8a1ab0ab3351",
+  measurementId: "G-S146F8DGPQ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
-export const storage =getStorage(app)
+export const storage = getStorage(app);
 const googleAuthProvider = new GoogleAuthProvider();
 const githubAuthProvider = new GithubAuthProvider();
 const twitterAuthProvider = new TwitterAuthProvider();
@@ -40,23 +55,23 @@ auth.useDeviceLanguage();
 // const analytics = getAnalytics(app);
 
 export {
-    auth,
-    createUserWithEmailAndPassword,
-    updateProfile,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut,
-    db,
-    addDoc,
-    collection,
-    onSnapshot,
-    query,
-    where,
-    googleAuthProvider,
-    signInWithPopup,
-    githubAuthProvider,
-    twitterAuthProvider,
-    doc,
-    getDoc,
-    setDoc,
-}
+  auth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  db,
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  where,
+  googleAuthProvider,
+  signInWithPopup,
+  githubAuthProvider,
+  twitterAuthProvider,
+  doc,
+  getDoc,
+  setDoc,
+};
