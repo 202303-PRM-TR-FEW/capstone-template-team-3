efd0d8ef079a54e3b1e7c52984f0d882f25f3c1c
Hunc Tasci <hunc.tasci@gmail.com>
2023-07-29 00:22:45 +0300

    added filtering functionality

--- app/campaigns/page.jsx
+++ app/campaigns/page.jsx
@@ -1,34 +1,35 @@
 "use client";
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { collection, query, onSnapshot, where } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 import CategoryFilter from "../components/CategoryFilter/CategoryFilter";
 import LargeCard from "../components/LargeCard/LargeCard";
-import Card from "../components/Card/Card.jsx";
-import {
-  collection,
-  getDoc,
-  query,
-  querySnapshot,
-  onSnapshot,
-} from "firebase/firestore";
-import { db } from "../firebase/firebase";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { ca } from "date-fns/locale";
+import Card from "../components/Card/Card";
 
 const Campaigns = () => {
+  const { push } = useRouter();
+  const searchParams = useSearchParams();
   const [campaigns, setCampaigns] = useState([]);
-  const router = useRouter();
+
+  const category = searchParams.get("category");
+  const filteredCampaignsByCategory = getCampaignsByCategory(
+    campaigns,
+    category
+  );
 
   // Read campaigns from database
   useEffect(() => {
     const q = query(collection(db, "campaigns"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let campaignsArr = [];
-
       querySnapshot.forEach((campaign) => {
         campaignsArr.push({ ...campaign.data(), id: campaign.id });
       });
       setCampaigns(campaignsArr);
     });
+
+    return () => unsubscribe();
   }, []);
 
   return (
@@ -36,10 +37,10 @@ const Campaigns = () => {
       <LargeCard />
       <CategoryFilter />
       <div className="flex flex-row gap-4 flex-wrap items-center justify-evenly container w-11/12 mx-auto">
-        {campaigns.map((campaign, index) => (
+        {filteredCampaignsByCategory.map((campaign) => (
           <div
-            key={index}
-            onClick={() => router.push(`/campaigns/${campaign.id}`)}
+            key={campaign.id}
+            onClick={() => push(`/campaigns/${campaign.id}`)}
           >
             <Card
               img={campaign.image}
@@ -52,6 +53,16 @@ const Campaigns = () => {
       </div>
     </main>
   );
+};
+
+const getCampaignsByCategory = (campaigns, category) => {
+  if (campaigns && campaigns.length > 0 && category) {
+    const filteredCampaigns = campaigns.filter((campaign) => {
+      return campaign.category?.map((item) => item.value).includes(category);
+    });
+    return filteredCampaigns;
+  }
+  return campaigns;
 };
 
 export default Campaigns;
