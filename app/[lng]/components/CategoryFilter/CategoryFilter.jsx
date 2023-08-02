f629b2fb1ba0527a4d65abfbe71de40397894fdb
Berkay <berkaybideci@gmail.com>
2023-08-02 13:46:11 +0300

    Update CategoryFilter.jsx

--- app/[lng]/components/CategoryFilter/CategoryFilter.jsx
+++ app/[lng]/components/CategoryFilter/CategoryFilter.jsx
@@ -27,7 +27,7 @@ const CategoryFilter = ({ lng }) => {
         <Link
           key={category.name}
           href={{
-            pathname: "/en/campaigns",
+            pathname: `/${lng}/campaigns`,
             query: { category: category.query },
           }}
           className="icon-main"
