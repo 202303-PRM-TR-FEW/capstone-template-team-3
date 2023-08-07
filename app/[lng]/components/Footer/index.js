ffa36212f8a108edc8a8e2c7910e6f2ad1adbdae
Berkay Bideci <berkaybideci@gmail.com>
2023-08-07 17:47:33 +0300

    Fix:  import client side for footer due to async/await server component error

--- app/[lng]/components/Footer/index.js
+++ app/[lng]/components/Footer/index.js
@@ -1,11 +1,7 @@
-"use client"
-
-import { usePathname } from 'next/navigation'
 import { useTranslation } from "../../../i18n";
 import { FooterBase } from "./FooterBase";
 
 export const Footer = async ({ lng }) => {
-  const pathname = usePathname()
   const { t } = await useTranslation(lng, "footer");
-  return <FooterBase t={t} lng={lng} pathname={pathname} />;
+  return <FooterBase t={t} lng={lng} />;
 };
